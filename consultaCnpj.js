73d25cdf949c55c7b4c03677489415ff0d6620af
jos3duardo <jos3duardolopes@gmail.com>
2020-04-27 23:16:16 -0400

    criando request de teste

---
+++ consultaCnpj.js
@@ -0,0 +1,10 @@
+let CNPJ = require('./lib/cnpj');
+let cnpj = new CNPJ();
+
+cnpj.consultaCNPJ({cnpj:'27865757000105'})
+.then(result => {
+    console.log(result)
+})
+.catch(error => {
+    console.log(error)
+})
