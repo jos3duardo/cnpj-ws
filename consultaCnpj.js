d08437167358f1cea00e673535c8b3bfdbcdb82f
rwietter <mauriciobw17@gmail.com>
2022-01-18 22:05:14 -0300

    chore: solve error in cpnj args

--- consultaCnpj.js
+++ consultaCnpj.js
@@ -1,7 +1,7 @@
 let CNPJ = require('./lib/cnpj');
 let cnpj = new CNPJ();
 
-cnpj.consultaCNPJ({cnpj:'27865757000105'})
+cnpj.consultaCNPJ({ cnpj: 27865757000105 })
 .then(result => {
     console.log(result)
 })
