ebd7c4b9c3f16dc89a20972dc98398ad1b392889
jos3duardo <jos3duardolopes@gmail.com>
2025-10-30 19:48:54 -0400

    refactor: update CNPJ consultation syntax and improve formatting

--- consultaCnpj.js
+++ consultaCnpj.js
@@ -1,10 +1,11 @@
-let CNPJ = require('./lib/cnpj');
+let CNPJ = require("./lib/cnpj");
 let cnpj = new CNPJ();
 
-cnpj.consultaCNPJ({cnpj:'27865757000105'})
-.then(result => {
-    console.log(result)
-})
-.catch(error => {
-    console.log(error)
-})
+cnpj
+  .consultaCNPJ("33000167000101")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
